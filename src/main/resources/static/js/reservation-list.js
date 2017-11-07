d1bb355aad7d996edf47a2c146c757de7fc07fa1
hnorbert90 <hnorbert90@gmail.com>
2017-11-07 19:14:25 +0100

    changed request for list all reservations for a period, and sorted by start date

--- src/main/resources/static/js/reservation-list.js
+++ src/main/resources/static/js/reservation-list.js
@@ -16,17 +16,26 @@ $(document).ready(function() {
     
     var reservationListTemplateSource = $("#reservation-list-template").html();      // get the template's html source
     var reservationListTemplate = Handlebars.compile(reservationListTemplateSource); // initialize Handlebars template
-
+    
+    var currentTime = new Date();
+	const DAYS = 30; 
+	var afterThirtyDays=new Date();
+	  	afterThirtyDays.setDate(afterThirtyDays.getDate() + DAYS);
+	  	
     $.ajax({
-      url : "/api/reservations/",
+      url : "http://localhost/api/reservations/from-"+currentTime.toISOString().substring(0, 10)+"/to-"+afterThirtyDays.toISOString().substring(0, 10)+"/",
       dataType : 'json',
       async : true, 
       cache : false,
       timeout : 5000, 
 
       data : {},
-      success : function(response) {
-
+      success : function(response) { 
+    	  
+        response.content.sort(function(a, b) { //sort by startDate
+        	return -(new Date(b.startDate) - new Date(a.startDate));
+    	});
+        
         var h = reservationListTemplate(response);     // generate HTML from the object using the template
         $("#reservation-list").empty();
         $("#reservation-list").append(h);              // insert the generated HTML into the document
